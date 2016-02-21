70deffcd265e39bf1eceda20a61a38c6c27c2528
gingerkitty <gingerkitty@me.com>
2016-02-21 03:26:10 -0800

    used handlebars to add partials to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,14 @@ var rename = require('gulp-rename');
 
 /* command to watch html template files for changes */
 gulp.task('templates', function(){
-    return gulp.src(['src/templates/**/*.hbs'])
-        .pipe(handlebars())
+    var data = {};
+
+    var options = {
+        batch: ['src/templates/partials']
+    }
+
+    return gulp.src(['src/templates/**/*.hbs','!src/templates/partials/**/*.hbs'])
+        .pipe(handlebars(data, options))
         .pipe(rename(function (path){
             path.extname = '.html'
         }))
