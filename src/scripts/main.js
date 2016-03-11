e90d7f539e344a19274fc9b0fb0b487c77d4117f
gingerkitty <gingerkitty@me.com>
2016-03-11 01:27:07 -0800

    setup of initial js structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,23 @@
-console.log('HELLO WORLD');
-console.log('HOWS IT GOING WORLD?');
+var model = {
+  init: function() {
+    console.log('Model initialized');
+  }
+};
+
+var helper = {
+
+};
+
+var view ={
+  init: function() {
+    console.log('View initialized');
+  }
+};
+var app = {
+  init: function() {
+    model.init();
+    view.init();
+  }
+};
+
+app.init();
